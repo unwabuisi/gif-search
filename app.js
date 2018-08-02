9d25c8842593440ac2d6a9b6b41c022621f146d6
unwabuisi <unwabuisi@utexas.edu>
2018-08-01 21:24:01 -0500

    api HTTPS fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var trendingURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+    var trendingURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
 
     // Adds buttons with trending Gifs
     $.ajax({
